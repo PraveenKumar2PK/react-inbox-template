629eddd6f15d6da32c95771a95146f769a6517ff
Praveen kumar <erodepraveen94@gmail.com>
2020-09-23 21:23:43 +0530

    Updates

---
+++ precache-manifest.70a9106f7ac67ca5d536550dbfffc9d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3680cd226b53dbbc985b22d8f2a78473",
+    "url": "/react-inbox-template/index.html"
+  },
+  {
+    "revision": "53b9da04cabd50e6f0e2",
+    "url": "/react-inbox-template/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "26ac50297847bfb26361",
+    "url": "/react-inbox-template/static/css/main.2470c274.chunk.css"
+  },
+  {
+    "revision": "53b9da04cabd50e6f0e2",
+    "url": "/react-inbox-template/static/js/2.66364ece.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-inbox-template/static/js/2.66364ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ac50297847bfb26361",
+    "url": "/react-inbox-template/static/js/main.6970b3a7.chunk.js"
+  },
+  {
+    "revision": "56f2a100f092d0cd5f6e",
+    "url": "/react-inbox-template/static/js/runtime-main.fb2d32fc.js"
+  }
+]);
